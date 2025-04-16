5d32e6b36f8e427d74c5091e8e186a7ba6d0f841
msjw-cloud <mohammed.alweatishi@gmail.com>
2025-04-16 16:40:47 +0300

    creating the action function

--- Environment.py
+++ Environment.py
@@ -8,3 +8,31 @@ class Jungle_Environment:
         self.Needle_num = Needle_num
         self.F_orientation = F_orientation
         self.state = (self.ambulance, self.F_orientation, self.Needle_num, False, 2, self.Shed_init_location)
+
+    def action(self, state):
+        available_actions = ['move-forward', 'turn-left', 'turn-right', 'throw-needle', 'pick', 'stay']
+        '''dealing with (move-forward) action'''
+        s = list(state)
+        actions = {
+            'up': lambda x: (x[0]+1, x[1]),
+            'down': lambda x: (x[0]-1, x[1]),
+            'right': lambda x: (x[0], x[1]+1),
+            'left': lambda x: (x[0], x[1]-1)
+        }
+        new_location = actions.get(s[1])(s[0])
+        if new_location in self.Trees_location:
+            available_actions.remove('move-forward')
+        elif new_location[0] >= self.N or new_location[0] < 0:
+            available_actions.remove('move-forward')
+        elif new_location[1] >= self.N or new_location[1] < 0:
+            available_actions.remove('move-forward')
+        '''dealing with (pick) action'''
+        if s[0] != s[5]:
+            available_actions.remove('pick')
+        '''dealing with (throw-needle)'''
+        if s[2] <= 0:
+            available_actions.remove('throw-needle')
+        '''return the set of actions available'''
+        return available_actions
+    
+    def result(self, state, action):
